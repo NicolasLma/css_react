b2ac5619e537a3c287c5b21c0c95699e57bd3b45
NICOLAS LOMBA <nicolaslomba82@gmail.com>
2024-04-07 21:38:33 +0200

    finished

---
+++ src/components/Card.jsx
@@ -0,0 +1,18 @@
+export default function Card() {
+  return (
+    <>
+      <div className="container">
+        <figure className="card">
+          <img
+            src="https://m.media-amazon.com/images/I/91wi6GgdW2L._UF1000,1000_QL80_.jpg"
+            alt="Sum 41"
+            className="card-img"
+          />
+
+          <h1>Sum 41</h1>
+          <p>Heaven x Hell ( 2024 )</p>
+        </figure>
+      </div>
+    </>
+  );
+}
